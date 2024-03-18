b7040c2fd666eebb57e72a8a3971c95a53bf8070
KaneWatson <webitgenius@gmail.com>
2024-03-18 01:24:28 +0100

    added react-helmet for SEO

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,15 @@
+import { Helmet } from "react-helmet-async"
 import ContactForm from "../components/ContactForm"
 
 function Contact() {
   return (
     <div className="flex gap-6 flex-col md:mx-56">
+      <Helmet prioritizeSeoTags>
+        <meta charSet="utf-8" />
+        <title>Web & IT Genius | Contact</title>
+        <meta name="description" content="Wanna get in touch? You can do so via the contact form below" />
+        <link rel="canonical" href="https://webitgenius.com/contact" />
+      </Helmet>
       <ContactForm />
     </div>
   )
