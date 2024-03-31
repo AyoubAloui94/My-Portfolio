faa284cec6b05506e527db3202b2d2fba91e4b15
KaneWatson <webitgenius@gmail.com>
2024-03-31 04:59:41 +0200

    added style for extra large screens again2

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,7 +3,7 @@ import ContactForm from "../components/ContactForm"
 
 function Contact() {
   return (
-    <div className="flex flex-col flex-grow sm:mx-32 md:mx-44 lg:mx-60 xl:mx-96 md:min-h-[70dvh] xl:min-h-[80dvh]">
+    <div className="flex flex-col flex-grow sm:mx-32 md:mx-44 lg:mx-60 xl:mx-96 md:min-h-[70dvh] xl:min-h-[70dvh]">
       <Helmet prioritizeSeoTags>
         <meta charSet="utf-8" />
         <title>alouiayoub.com | Contact</title>
