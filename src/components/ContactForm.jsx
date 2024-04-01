e04416d7b1dd65748505fb5c20d25794bacdf842
KaneWatson <webitgenius@gmail.com>
2024-04-02 00:10:12 +0200

    switched turnstile for hcaptcha

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -3,40 +3,46 @@ import FormRow from "./FormRow"
 import emailjs from "@emailjs/browser"
 import { toast } from "react-hot-toast"
 import SpinnerMini from "./SpinnerMini"
-// import ReCAPTCHA from "react-google-recaptcha"
-import Turnstile, { useTurnstile } from "react-turnstile"
+import HCaptcha from "@hcaptcha/react-hcaptcha"
 
 function ContactForm() {
   const nameRef = useRef()
   const emailRef = useRef()
   const messageRef = useRef()
-  const turnstile = useTurnstile()
   const captchaRef = useRef()
+  // const [token, setToken] = useState(null)
 
   const [isLoading, setIsLoading] = useState(false)
 
-  async function handleSubmit(e) {
+  function handleSubmit(e) {
     e.preventDefault()
+    setIsLoading(true)
+
+    const [name, email, message] = [nameRef.current.value, emailRef.current.value, messageRef.current.value]
+
+    if (!name || !email || !message) return
+
+    captchaRef.current?.execute()
+  }
+
+  async function handleEmail(token) {
     try {
-      const [name, email, message] = [nameRef.current.value, emailRef.current.value, messageRef.current.value]
-      if (!name || !email || !message) return
-      const token = captchaRef.current.childNodes[1].value
-      if (!token) throw new Error("reCaptcha invalid")
+      if (!token) throw new Error("captcha invalid")
+      setIsLoading(true)
+
       const params = {
         user_name: nameRef.current.value,
         user_email: emailRef.current.value,
         message: messageRef.current.value,
         to_name: "Ayoub"
       }
-      setIsLoading(true)
       await emailjs.send(import.meta.env.VITE_SERVICE_ID, import.meta.env.VITE_EMAILJS_TEMPLATE_ID, params, {
-        publicKey: import.meta.env.VITE_EMAILJS_PUBLIC_KEY,
-        limitRate: {
-          throttle: 300000
-        }
+        publicKey: import.meta.env.VITE_EMAILJS_PUBLIC_KEY
       })
 
       toast.success("Message successfully sent!")
+
+      captchaRef.current?.resetCaptcha()
 
       nameRef.current.value = ""
       emailRef.current.value = ""
@@ -63,9 +69,18 @@ function ContactForm() {
       <FormRow name="message" id="message" type="text" placeholder="Your message" ref={messageRef} isTextArea={true} required>
         Message*
       </FormRow>
-      <div className="flex justify-center">
-        {/* <ReCAPTCHA sitekey={import.meta.env.VITE_RECAPTCHA_SITE_KEY} ref={recaptchaRef} style={{ minHeight: "4.85rem" }} /> */}
-        <Turnstile sitekey={import.meta.env.VITE_SITE_KEY} onExpire={() => turnstile.reset()} fixedSize={true} theme="dark" userRef={captchaRef} />
+      <div className="flex justify-center h-min-[4.5rem]">
+        <HCaptcha
+          loadAsync={true}
+          sitekey={import.meta.env.VITE_HCAPTCHA_SITE_KEY}
+          onVerify={token => {
+            handleEmail(token)
+          }}
+          size="invisible"
+          ref={captchaRef}
+          onClose={() => setIsLoading(false)}
+          onError={captchaRef.current?.resetCaptcha()}
+        />
       </div>
       <button type="submit" className="bg-[#4f46e5] text-gray-100 py-3 text-lg font-bold w-24 mx-7 rounded-2xl self-center text-center" disabled={isLoading}>
         {isLoading ? <SpinnerMini /> : "Submit"}
