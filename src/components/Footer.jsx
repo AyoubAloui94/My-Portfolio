84344a432b2ad2001c32263981dd721d796a8ee7
KaneWatson <webitgenius@gmail.com>
2024-04-02 02:56:26 +0200

    added twitterto my socials

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -21,6 +21,11 @@ function Footer() {
                 <path d="M41,4H9C6.24,4,4,6.24,4,9v32c0,2.76,2.24,5,5,5h32c2.76,0,5-2.24,5-5V9C46,6.24,43.76,4,41,4z M17,20v19h-6V20H17z M11,14.47c0-1.4,1.2-2.47,3-2.47s2.93,1.07,3,2.47c0,1.4-1.12,2.53-3,2.53C12.2,17,11,15.87,11,14.47z M39,39h-6c0,0,0-9.26,0-10 c0-2-1-4-3.5-4.04h-0.08C27,24.96,26,27.02,26,29c0,0.91,0,10,0,10h-6V20h6v2.56c0,0,1.93-2.56,5.81-2.56 c3.97,0,7.19,2.73,7.19,8.26V39z" fill="#fff"></path>
               </svg>
             </Link>
+            <Link title="twitter" to="https://twitter.com/AyoubAloui37260" target="_blank" rel="noopener noreferrer">
+              <svg fill="none" height="24" width="24" xmlns="http://www.w3.org/2000/svg" viewBox="0.254 0.25 500 451.95400000000006">
+                <path d="M394.033.25h76.67L303.202 191.693l197.052 260.511h-154.29L225.118 294.205 86.844 452.204H10.127l179.16-204.77L.254.25H158.46l109.234 144.417zm-26.908 406.063h42.483L135.377 43.73h-45.59z" fill="#fff" />
+              </svg>
+            </Link>
           </div>
         </div>
         <span className="uppercase text-xs font-semibold">
